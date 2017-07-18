9aa07dfd5265bdb85e51bd081df2c2828b793598
Matt <mattwalker216@gmail.com>
2017-07-18 01:28:52 +0000

    Lesson Permissions (challenge)

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,8 +1,17 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_lessons, only: [:show]
+
   def show
   end
 
   private
+
+  def require_authorized_for_lessons
+    if current_user.enrolled_in?(current_lesson.section.course) != true
+      redirect_to course_path(current_lesson.section.course), alert: 'Sorry, but you must be enrolled in the course to see the lessons'
+    end
+  end
 
   helper_method :current_lesson
   def current_lesson
